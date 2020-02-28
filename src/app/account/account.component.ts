7ab0b10a04d88fcc6f1cb44f7547319a218fd6e7
Tosun Sait <tosunsa@cronos.be>
2020-02-28 10:34:47 +0100

    first and last injecting services into services

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
+import { AccountsService } from './../accounts.service';
 import { LoggingService } from './../logging.service';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -9,15 +10,15 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class AccountComponent implements OnInit {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private loggingService: LoggingService,
+              private accountsService: AccountsService) { }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    this.loggingService.logStatusChange(status);
+    this.accountsService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChange(status);
   }
 
-  constructor(private loggingService: LoggingService) { }
 
   ngOnInit(): void {
   }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
+import { AccountsService } from './../accounts.service';
 import { LoggingService } from './../logging.service';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-account',
@@ -7,17 +8,15 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent implements OnInit {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  
+  constructor(private loggingService: LoggingService,
+              private accountsService: AccountsService) { }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    this.loggingService.logStatusChange(accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus)
+    // this.loggingService.logStatusChange(accountStatus);
   }
 
-  constructor(private loggingService: LoggingService) { }
 
   ngOnInit(): void {
   }
