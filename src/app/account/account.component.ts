3de5eb1c274d11002201b3fb93fb9f5d29269f38
Tosun Sait <tosunsa@cronos.be>
2020-02-28 09:27:35 +0100

    initial commit

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,25 @@
+import { LoggingService } from './../logging.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent implements OnInit {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+
+
+  onSetTo(status: string) {
+    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.loggingService.logStatusChange(status);
+  }
+
+  constructor(private loggingService: LoggingService) { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,25 @@
+import { LoggingService } from './../logging.service';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent implements OnInit {
+  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountAdded.emit({
+      name: accountName,
+      status: accountStatus
+    });
+    this.loggingService.logStatusChange(accountStatus);
+  }
+
+  constructor(private loggingService: LoggingService) { }
+
+  ngOnInit(): void {
+  }
+
+}
